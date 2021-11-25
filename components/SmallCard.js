718d80f0f6d906729fe9281c925bfac7cbdd6db2
Xiaowei Xie <xiaowxie@microsoft.com>
2021-11-25 19:00:53 +0800

    chore: save

--- components/SmallCard.js
+++ components/SmallCard.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 export default function SmallCard({ Icon, title, slug }) {
+  const width = 153
+  const height = 200
   return (
     <a className="card-small" href={`/project/${slug}`}>
-      <Icon w={153} h={163} />
+      <Icon w={width} h={height} />
       <Icon w={153} h={163} />
       <h3>{title}</h3>
     </a>
